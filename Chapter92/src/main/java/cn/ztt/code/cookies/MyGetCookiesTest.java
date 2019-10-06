3b78ee591188bc12ff470bda32fe5a02f8060658
zhutingting <tingting.zhu@thoughtworks.com>
2019-10-07 01:25:26 +0800

    [Chapter9] test get request with cookies

--- Chapter92/src/main/java/cn/ztt/code/cookies/MyGetCookiesTest.java
+++ Chapter92/src/main/java/cn/ztt/code/cookies/MyGetCookiesTest.java
@@ -28,7 +28,7 @@ public class MyGetCookiesTest {
     }
 
     @Test
-    public void getCookies() throws IOException {
+    public void getCookiesTest() throws IOException {
         String testUrl = url + resourceBundle.getString("getCookies.uri");
         DefaultHttpClient client = new DefaultHttpClient();
         HttpGet get = new HttpGet(testUrl);
@@ -36,13 +36,30 @@ public class MyGetCookiesTest {
         String result = EntityUtils.toString(response.getEntity(), "utf-8");
         System.out.println(result);
 
-        store = client.getCookieStore();
+        this.store = client.getCookieStore();
         List<Cookie> cookies = store.getCookies();
         for (Cookie cookie : cookies) {
             String name = cookie.getName();
             String value = cookie.getValue();
             System.out.println("cookie name = "+name+", cookie value="+value);
 
+        }
+    }
+
+    @Test(dependsOnMethods ={"getCookiesTest"})
+    public void getWithCookiesTest() throws IOException {
+        String testUrl = url + resourceBundle.getString("test.get.with.cookies");
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpGet get = new HttpGet(testUrl);
+        client.setCookieStore(this.store);
+        HttpResponse response = client.execute(get);
+
+
+        int statusCode = response.getStatusLine().getStatusCode();
+        System.out.println("statusCode = " + statusCode);
+        if(statusCode == 200){
+            String result = EntityUtils.toString(response.getEntity(),"utf-8");
+            System.out.println(result);
         }
     }
 }
