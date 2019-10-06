24d9fedc6c90ad7a4b920d0e8186c543ed303976
zhutingting <tingting.zhu@thoughtworks.com>
2019-10-06 14:05:33 +0800

    [Knowledge] Parameters annotation and xml config to provide data

---
+++ Chapter5/src/main/java/cn/ztt/code/parameter/ParameterTest.java
@@ -0,0 +1,12 @@
+package cn.ztt.code.parameter;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ParameterTest {
+    @Test
+    @Parameters({"name","age"})
+    public void parameterTest1(String name, int age) {
+        System.out.println("name = "+name+", age="+age);
+    }
+}
