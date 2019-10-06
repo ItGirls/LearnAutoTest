6417bedf3af6514dc2ed96284da33642ebb15186
zhutingting <tingting.zhu@thoughtworks.com>
2019-10-06 14:19:56 +0800

    [Knowledge] provide data depends on method name

--- Chapter5/src/main/java/cn/ztt/code/parameter/DataProviderTest.java
+++ Chapter5/src/main/java/cn/ztt/code/parameter/DataProviderTest.java
@@ -3,6 +3,8 @@ package cn.ztt.code.parameter;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Method;
+
 public class DataProviderTest {
 
     @Test(dataProvider ="data")
@@ -26,4 +28,31 @@ public class DataProviderTest {
 
         return o;
     }
+
+    @Test(dataProvider = "methodDataProvider")
+    public void testCase1(String name, int age) {
+        System.out.println("name = "+name+", age="+age);
+    }
+
+    @Test(dataProvider = "methodDataProvider")
+    public void testCase2(String name, int age) {
+        System.out.println("name = "+name+", age="+age);
+    }
+
+    @DataProvider(name = "methodDataProvider")
+    public Object[][] providerData1(Method method) {
+        Object[][] result = null;
+        if (method.getName().equals("testCase1")) {
+            result = new Object[][]{
+                    {"zhangsan",13}
+            };
+        } else if (method.getName().equals("testCase2")) {
+            result = new Object[][]{
+                    {"lisi",14},
+                    {"wangwu",15}
+            };
+        }
+        return result;
+    }
+
 }
