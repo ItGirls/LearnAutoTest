ca286809251efa6db3bd412e8dc3cdd015d448d7
zhutingting <tingting.zhu@thoughtworks.com>
2019-10-06 10:28:02 +0800

    [Knowledge]xml config under multiple test files

---
+++ Chapter5/src/main/java/cn/ztt/code/suite/LoginTest.java
@@ -0,0 +1,11 @@
+package cn.ztt.code.suite;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void loginTest() {
+        System.out.println("登录成功.");
+    }
+}
---
+++ Chapter5/src/main/java/cn/ztt/code/suite/PayTest.java
@@ -0,0 +1,11 @@
+package cn.ztt.code.suite;
+
+import org.testng.annotations.Test;
+
+public class PayTest {
+
+    @Test
+    public void payTest() {
+        System.out.println("支付成功.");
+    }
+}
