bb5e22dc2e91519b145c2a5a146b76b0494bcb35
zhutingting <tingting.zhu@thoughtworks.com>
2019-10-07 16:22:27 +0800

    [Chapter10] post

---
+++ Chapter101/src/main/java/cn/ztt/code/server/MyPostMethod.java
@@ -0,0 +1,34 @@
+package cn.ztt.code.server;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@Api(value = "/")
+@RequestMapping("/v1")
+public class MyPostMethod {
+
+    //用于cookies信息的
+    private static Cookie cookie;
+
+    //用户登陆成功获取到cookies，然后再访问其他接口获取到列表
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    @ApiOperation(value = "登陆接口，成功后获取cookies信息",httpMethod = "POST")
+    public String login(HttpServletResponse response,
+                        @RequestParam(value = "userName",required = true) String userName,
+                        @RequestParam(value = "password",required = true)  String password){
+        if(userName.equals("zhangsan") && password.equals("123456")){
+            cookie = new Cookie("login","true");
+            response.addCookie(cookie);
+            return "恭喜你登陆成功了!";
+        }
+        return "用户名或者是密码错误！";
+    }
+}
